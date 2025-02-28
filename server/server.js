7df2ad1487c52415049c403e3a78aec78416cd86
Daniel <50241108+daniel423615@users.noreply.github.com>
2025-02-28 20:59:20 +0100

    Borrador pruebas principal.html
    Se añade código para probar que el servidor esta accediendo correctamente a la base de datos y que devuelve los datos.

--- server/server.js
+++ server/server.js
@@ -2,11 +2,15 @@ const express = require('express');
 const pool = require('./config/db.js'); // Importar la conexión a PostgreSQL
 const usuarioRoutes = require('./routes/usuarioRoutes');
 const taskRoutes = require('./routes/taskRoutes');
+const path = require('path'); // Necesario para trabajar con rutas de archivos
 const app = express();
 const port = 3000;  // Puedes cambiar el puerto si lo deseas
 
 // Middleware para procesar JSON
 app.use(express.json());
+
+// Servir archivos estáticos desde la carpeta 'src/public'
+app.use(express.static(path.join(__dirname, '../src/public')));
 
 // Ruta para obtener datos desde PostgreSQL
 app.use('/api/tasks', taskRoutes);
