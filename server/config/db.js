c2f4d74a83e4a9c3d5bf2e596ffef16c9089b1f9
Daniel <50241108+daniel423615@users.noreply.github.com>
2025-03-09 22:37:11 +0100

    Esta no era o_o

--- server/config/db.js
+++ server/config/db.js
@@ -3,15 +3,12 @@ const { Pool } = require('pg');
 
 // Configurar la conexión con PostgreSQL
 const pool = new Pool({
-  connectionString: process.env.DB_STRING
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
 });
-// const pool = new Pool({
-//   user: process.env.DB_USER,
-//   host: process.env.DB_HOST,
-//   database: process.env.DB_NAME,
-//   password: process.env.DB_PASSWORD,
-//   port: process.env.DB_PORT
-// });
 
 pool.connect()
   .then(() => console.log('Conectado a PostgreSQL'))
